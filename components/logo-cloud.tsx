b2d4dd50087c396a8093a1bd1183bd8bdeeb6efc
Anas Salem <anassalem.aa@gmail.com>
2026-03-19 04:38:54 +0400

    chore: Replace ESLint with Oxlint.

--- components/logo-cloud.tsx
+++ components/logo-cloud.tsx
@@ -18,7 +18,7 @@ export function LogoCloud({ className, logos, ...props }: LogoCloudProps) {
     <div
       {...props}
       className={cn(
-        'overflow-hidden py-4 [mask-image:linear-gradient(to_right,transparent,black,transparent)]',
+        'overflow-hidden py-4 mask-[linear-gradient(to_right,transparent,black,transparent)]',
         className
       )}>
       <InfiniteSlider gap={42} reverse speed={80} speedOnHover={25}>
